ae04fa20aadf83d564369cfdee6629796e7c036e
gguerrero11 <gguerrero11@gmail.com>
2015-02-07 14:02:28 -0700

    Initial Commit

---
+++ ScoreKeeper-TableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScoreKeeper-TableView
+//
+//  Created by Gabriel Guerrero on 2/7/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScoreKeeper-TableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreKeeper-TableView
+//
+//  Created by Gabriel Guerrero on 2/7/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
